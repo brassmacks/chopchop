e2d58712c7b08a5b1651db476aae62faabf809c5
Joshua Elliott <jadepavedpath@gmail.com>
2021-07-13 15:26:27 -0700

    "debugged knife draw obj"

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -1,16 +1,16 @@
 // require 
-const clickMap = docuent.querySelector("div.canvas");
-const canEl = document.querySelector("canvas#mycanvas");
-const ctx = canEl.getContext('2d');
-const canHi = canEl.height;
-const canWi = canEl.width;
+// const clickMap = document.querySelector("div.canvas");
+// const canEl = document.querySelector("canvas#mycanvas");
+// const ctx = canEl.getContext('2d');
+// const canHi = canEl.height;
+// const canWi = canEl.width;
 
 
 
 
 
 
-let knife = function() {
+const Knife = function() {
   
   const wid = 15;
   let pntx = canWi * 0.8;
@@ -31,11 +31,11 @@ let knife = function() {
   this.point = [pntx,pnty];
   this.handle = [hanx, hany];
   this.blade = [this.point, this.handle];
-  this.hilt = {
-    inBot: relativeTurn(this.handle, wid, 90),
-    inTop: relativeTurn(inBot, this.spine.length * 0.3, 90),
-    outTop: relativeTurn(inTop, wid, 90),
-    outBot: relativeTurn(outTop, this.spine.length * 0.3, 90)
+  this.hilt = function() {
+    let inBot = relativeTurn(this.handle, wid, 90);
+    let inTop = relativeTurn(this.hilt[inBot], this.spine.length * 0.3, 90);
+    let outTop = relativeTurn(inTop, wid, 90);
+    let outBot = relativeTurn(outTop, this.spine.length * 0.3, 90);
   };
   // r will change, it is currently for rendering purposes only
   // it represents half the width of the knife objec
@@ -74,43 +74,43 @@ can.addEventListener('mouseenter', function (e) {
 });
 
 
-document.addEventListener("keypress", function (e) {
-  let x = (can.height * 0.66);
-  const xS = { "a": x - 50, "s": x, "d": x + 50 };
-  let y = can.width * 0.33;
-  let flash;
-  let c1 = "rgb(20,30,250)";
-  let c2 = "rgb(40,250,25)";
-  let c3 = "rgb(250,15,55)";
+// document.addEventListener("keypress", function (e) {
+//   let x = (can.height * 0.66);
+//   const xS = { "a": x - 50, "s": x, "d": x + 50 };
+//   let y = can.width * 0.33;
+//   let flash;
+//   let c1 = "rgb(20,30,250)";
+//   let c2 = "rgb(40,250,25)";
+//   let c3 = "rgb(250,15,55)";
 
-  switch (e.key) {
-    case "a":
-      x -= 50;
-      flash = c1;
-      break;
-    case "s":
-      flash = c2;
-      break;
-    case "d":
-      flash = c3;
-      x += 50;
-      break;
-    default:
-      flash = null;
-      console.log(flash);
-    // call mishap function
-  }
-  console.log(flash);
+//   switch (e.key) {
+//     case "a":
+//       x -= 50;
+//       flash = c1;
+//       break;
+//     case "s":
+//       flash = c2;
+//       break;
+//     case "d":
+//       flash = c3;
+//       x += 50;
+//       break;
+//     default:
+//       flash = null;
+//       console.log(flash);
+//     // call mishap function
+//   }
+//   console.log(flash);
 
-  ctx.fillStyle = flash || "transparent";
-  ctx.fillRect(x, y, 50, 50);
+//   ctx.fillStyle = flash || "transparent";
+//   ctx.fillRect(x, y, 50, 50);
 
-  document.addEventListener('keyup', function (e) {
-    if (["a", "s", "d"].includes(e.key)) {
-      let xC = xS[e.key];
-      ctx.clearRect(xC, y, 50, 50);
-    }
-  });
+//   document.addEventListener('keyup', function (e) {
+//     if (["a", "s", "d"].includes(e.key)) {
+//       let xC = xS[e.key];
+//       ctx.clearRect(xC, y, 50, 50);
+//     }
+//   }); 
 
-});
+// });
 
